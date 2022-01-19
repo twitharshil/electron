b24d80dd1d8a006692f0266d7b0236275ffa8952
Shelley Vohr <shelley.vohr@gmail.com>
2022-01-19 09:39:53 +0100

    chore: cancel active pending requests if denied

--- shell/browser/api/electron_api_web_contents.cc
+++ shell/browser/api/electron_api_web_contents.cc
@@ -1397,6 +1397,9 @@ void WebContents::RequestExclusivePointerAccess(
   if (allowed) {
     exclusive_access_manager_->mouse_lock_controller()->RequestToLockMouse(
         web_contents, user_gesture, last_unlocked_by_target);
+  } else {
+    web_contents->GotResponseToLockMouseRequest(
+        blink::mojom::PointerLockResult::kPermissionDenied);
   }
 }
 
